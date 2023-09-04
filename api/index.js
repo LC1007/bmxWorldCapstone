23e47a7bd5834dca9c7b4bcc26e6669084a2d76b
LC1007 <scholtz0929@gmail.com>
2023-09-04 23:23:08 +0200

    changes

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.use(
   express.urlencoded({ extended: false }),
   express.json(),
   cookieParse(),
-  cors({ origin: "http://localhost:8080/", credentials: true }),
+  cors(),
   routes
 );
 
